4150e6d2db104dc87cabd440778c6d3385ec74f1
Dell <Dell@DESKTOP-NAPL2JR>
2023-02-14 23:45:15 +0530

    commit

---
+++ xyz/src/test/java/xyz/TC001.java
@@ -0,0 +1,29 @@
+package xyz;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class TC001 {
+
+	public static void main(String args[])throws InterruptedException
+	{
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Documents\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+		Thread.sleep(2000);
+		WebElement search=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		search.sendKeys("mobile");
+
+
+		WebElement searchButton=driver.findElement(By.xpath("//input[@type='submit']"));
+		searchButton.click();
+	}
+}
